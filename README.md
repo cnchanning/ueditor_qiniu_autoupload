162038dc0a424c4ed1559326934fddd31ecf149b
channing <cn_channing@hotmail.com>
2015-11-09 17:50:08 +0800

    Ueditor 上传至七牛云

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ueditor_qiniu_autoupload
 ueditor自动上传图片至七牛CDN插件
+
+## useage
+1. 删除UEDitor自带的autoupload插件
+2. config.js中的配置项加入编辑器配置项目中
+3. 加载ueditor_qiniu_autoload.js
---
+++ qiniu/config.js
@@ -0,0 +1,6 @@
+var config={
+    	tokenUrl:"后端获取token地址"
+        ,qiniuUploadUrl:"http://upload.qiniu.com"
+        ,qiniuCDN:"CDN Path"
+        ,qiniuPrefixPath:"CDN路径前缀"
+}
